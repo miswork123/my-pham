f4b3e4249c4ac9d4f8c20b7ad9260710c1a19527
caonguyen <caonguyenvhit@gmail.com>
2023-03-24 16:12:45 +0700

    tab-section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,29 @@
-let titleQuestion = document.querySelectorAll('.list-menu');
-for (let i = 0; i < titleQuestion.length; i++) {
+// let titleQuestion = document.querySelectorAll('.list-menu');
+// for (let i = 0; i < titleQuestion.length; i++) {
 
-	titleQuestion[i].addEventListener('click', function() {
+// 	titleQuestion[i].addEventListener('click', function() {
 		
-		this.classList.toggle('active');
-	});
-}
+// 		this.classList.toggle('active');
+// 	});
+// }
+
+
+	const $ = document.querySelector.bind(document)
+	const $$ = document.querySelectorAll.bind(document)
+
+	const makeUp = $$('.tab-mekup')
+	const bests = $$('.best-seller-list')
+
+	makeUp.forEach((tab, index) =>{
+		const best = bests[index]
+
+		tab.onclick = function (){
+
+
+			$('.tab-mekup.active').classList.remove('active')
+			$('.best-seller-list.show').classList.remove('show')
+			
+			this.classList.add('active')
+			best.classList.add('show')
+		}
+	})
